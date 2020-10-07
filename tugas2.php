ddb60ae199a19409a3155e774566ce31c7cc490e
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-07 10:15:02 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<head>
+    <title>Tugas 2</title>
+</head>
+
+<body>
+<?php
+$_1 = 1000;
+$_2 = 15000;
+$_3 = 20000;
+$_4 = 5000;
+$_5 = 1000;
+
+?>
+
+
+<h1>DAFTAR MENU</h1>
+<table border = '1'>
+<tr>
+<td>No</td>
+<td>Nama Menu</td>
+<td>Harga</td>
+</tr>
+
+<tr>
+<td>1</td>
+<td>Cireng</td>
+<td>Rp. <?php echo $_1 ?></td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>Es Duren</td>
+<td>Rp. <?php echo $_2 ?></td>
+</tr>
+
+<tr>
+<td>3</td>
+<td>Es Milo</td>
+<td>Rp. <?php echo $_3 ?></td>
+</tr>
+
+<tr>
+<td>4</td>
+<td>Es Teh</td>
+<td>Rp. <?php echo $_4 ?></td>
+</tr>
+
+<tr>
+<td>5</td>
+<td>Piscok</td>
+<td>Rp. <?php echo $_5 ?></td>
+</tr>
+
+
+</table>
+</body>
+</html>
